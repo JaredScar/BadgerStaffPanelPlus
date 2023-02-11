620b1d0aca67dc4ac32d1dd41c50ae03d4dcac12
Jared <TheWolfBadger@gmail.com>
2023-02-11 15:29:17 -0500

    + More changes to LoginController handling Discord login potentially

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Staff;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -39,8 +40,49 @@ class LoginController extends Controller {
         return ['token' => $token];
     }
 
-    public function authenticateDiscord(Request $request) {
+    private function discordReq($url, $headers = array(), $data = array(), $post = false) {
+        $ch = curl_init($url);
+        $headers[] = 'Accept: application/json';
+        curl_setopt_array($ch, array(
+            CURLOPT_POST => $post,
+            CURLOPT_RETURNTRANSFER => TRUE,
+            CURLOPT_IPRESOLVE => CURL_IPRESOLVE_V4
+        ));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        $resp = curl_exec($ch);
+        return json_decode($resp);
+    }
+
+    public function authenticateDiscord(Request $request): RedirectResponse {
         $discordCode = $request->only('discordCode');
-        // TODO need to reach out to Discord API and make sure this user is valid and discord ID exists in DB for staff
+        $data = [
+            'client_id' => DISCORD_CLIENT_ID,
+            'client_secret' => DISCORD_CLIENT_SECRET,
+            'grant_type' => 'authorization_code',
+            'code' => $discordCode,
+            'redirect_uri' => DISCORD_REDIRECT_URI,
+            'scope' => 'identify email guilds guilds.join'
+        ];
+        $options =  [
+            'https' => [
+                'header' => 'Content-Type: application/x-www-form-urlencoded',
+                'content' => http_build_query($data)
+            ],
+            'http' => [
+                'header' => 'Content-Type: application/x-www-form-urlencoded',
+                'content' => http_build_query($data)
+            ],
+        ];
+        $discordResp = $this->discordReq('https://discord.com/api/oauth2/authorize', $options['https']);
+        $access_token = $discordResp['access_token'];
+        $user = $this->discordReq('https://discord.com/api/users/@me', ['Authorization: Bearer ' . $access_token]);
+        $staffMemberSelected = Staff::where('staff_discord', $user['id'])->first()->aggregate;
+        if (sizeof($staffMemberSelected) > 0) {
+            // It's a valid staff member, we need to log them in
+            // TODO
+        }
+        return back()->withErrors([
+            'username' => 'The provided credentials do not match our records...'
+        ]);
     }
 }
--- configuration.php
+++ configuration.php
@@ -1,28 +1,33 @@
 <?php
-$MASTER_ADMIN_DISCORD_ID = '';
-$MASTER_ADMIN_ROLE_ID = '';
+// Use https://www.lastpass.com/features/password-generator#generatorTool for generating this
+const MASTER_API_KEY = 'Adg2M3vTywkv5OmjMpL4iM9cIC'; // This should not be used... Use the API keys associated to accounts
 
-$DISCORD_BOT_TOKEN = "";
-$DISCORD_CLIENT_ID = "";
-$DISCORD_REDIRECT_URI = "";
-$GUILD_IDS = [
-    "",
+const MASTER_ADMIN_DISCORD_ID = '394446211341615104';
+const MASTER_ADMIN_ROLE_ID = '932655785404665949';
+
+const DISCORD_BOT_TOKEN = "";
+const DISCORD_CLIENT_ID = "";
+
+const DISCORD_CLIENT_SECRET = "";
+const DISCORD_REDIRECT_URI = "";
+const GUILD_IDS = [
+    "932655785190764564",
 ];
-$SERVERS = [
+const SERVERS = [
     [
-        'Server_Name' => '',
+        'Server_Name' => 'Badger Test Server',
         'Server_IP' => '',
         'Server_Port' => '',
         'RCON_Password' => ''
     ],
 ];
-$DISCORD_REDIRECT_AUTH = "";
+const DISCORD_REDIRECT_AUTH = "";
 
 /**
  * SQL Information
  */
-$SQL_HOST = "";
-$SQL_USERNAME = "";
-$SQL_PASSWORD = "";
-$SQL_DB = "";
-$SQL_PORT = 3306;
+const SQL_HOST = "";
+const SQL_USERNAME = "";
+const SQL_PASSWORD = "";
+const SQL_DB = "";
+const SQL_PORT = 3306;
