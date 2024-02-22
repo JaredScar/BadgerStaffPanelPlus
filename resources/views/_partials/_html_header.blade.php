3d18763b3eab1e4ba0ac0a5d10ced29df3466fbe
Jared <TheWolfBadger@gmail.com>
2024-02-21 21:12:58 -0500

    feat/dashboard-page-upgrades Change from gridster to GridStack

--- resources/views/_partials/_html_header.blade.php
+++ resources/views/_partials/_html_header.blade.php
@@ -8,10 +8,11 @@
     <link
         href="https://cdn.jsdelivr.net/npm/bootstrap5-toggle@5.0.4/css/bootstrap5-toggle.min.css"
         rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.gridster/0.5.6/jquery.gridster.min.css" integrity="sha512-RfPoqYhcoK5PP0OWrPp8Bva1j9VOWyEMYTM5LKoCtaRNl6X373XlnUGxrnFZ1qxNTUCIKk7tnSBZulVs8nfPIg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css" />
     <link rel="stylesheet" href="https://cdn.datatables.net/fixedheader/3.2.0/css/fixedHeader.dataTables.min.css" />
-    <link rel="stylesheet" href="js/jquery.gridster.min.css" />
+    <!-- Include GridStack CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/gridstack.js/10.1.0/gridstack.min.css" integrity="sha512-GEMqAs1Ai2iirp6QhD4N5Qt7tDn1RGgF1CY2xp57LrX3vqTDehSC88IOFewV/L84htjbN7aazcK10FyzH+eXSA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <link rel="stylesheet" href="{{ asset('css/global.css') }}" />
     <link rel="stylesheet" href="{{ asset('/css/' . $data['css_path'] . '.css') }}" />
 </head>
--- resources/views/verified/dashboard.blade.php
+++ resources/views/verified/dashboard.blade.php
@@ -17,13 +17,13 @@
                     </button>
                 </div>
             </div>
-            <div class="gridster flex-grow-1 row">
+            <div class="row flex-grow-1">
                 <div class="col col-12">
-                    <ul>
+                    <div class="grid-stack">
                         @foreach($data['widgetData'] as $widget)
-                            <li data-row="{{$widget['row']}}" data-col="{{$widget['col']}}"
-                                data-sizex="{{$widget['size_x']}}" data-sizey="{{$widget['size_y']}}">
-                                <div class="container-fluid h-100 px-0">
+                            <div class="grid-stack-item" gs-y="{{$widget['row']}}" gs-x="{{$widget['col']}}"
+                                gs-width="{{$widget['size_x']}}" gs-height="{{$widget['size_y']}}">
+                                <div class="container-fluid h-100 px-0 grid-stack-item-content">
                                     <div class="d-flex flex-column h-100">
                                         <div class="row bg-zap mx-0 widget-header">
                                             <div class="col-12 text-white">
@@ -37,9 +37,9 @@
                                         </div>
                                     </div>
                                 </div>
-                            </li>
+                            </div>
                         @endforeach
-                    </ul>
+                    </div>
                 </div>
             </div>
         </div>
@@ -48,22 +48,10 @@
 </div>
 <script>
     $(function () { //DOM Ready
-        const gridster = $(".gridster ul").gridster({
-            widget_base_dimensions: ['auto', 100],
-            widget_margins: [5, 5],
-            avoid_overlapped_widgets: true, // Don't allow widgets loaded from the DOM to overlap. This is helpful if you're loading widget positions form the database, and they might be inconsistent.
-            max_cols: 12,
-            autogenerate_stylesheet: true, // If true, all the CSS required to position all widgets in their respective columns and rows will be generated automatically and injected to the <head> of the document.
-            resize: {
-                enabled: true
-            },
-            shift_widgets_up: false,
-            shift_larger_widgets_down: false,
-            collision: {
-                wait_for_mouseup: true
+        GridStack.init(
+            {
             }
-        }).data('gridster');
-        gridster.disable();
+        );
     });
 </script>
 </body>
