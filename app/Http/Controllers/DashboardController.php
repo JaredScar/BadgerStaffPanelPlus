c288692682b4ea495588866372c14b6847a3456b
Jared <TheWolfBadger@gmail.com>
2024-03-10 14:49:21 -0400

    feat/dashboard-page-upgrades Added toasts partial

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,54 +3,59 @@
 namespace App\Http\Controllers;
 
 use App\Models\Layout;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 
 class DashboardController extends Controller {
     public function save(Request $request) {
         // Retrieve the JSON data from the request body
-        $widgetDataList = $request->json()->all();
-        $staffId = Session::get("staff_id");
+        try {
+            $widgetDataList = $request->json()->all();
+            $staffId = Session::get("staff_id");
 
-        $updated = date('Y-m-d H:i:s', time());
-        $existingWidgetIds = [];
-        foreach ($widgetDataList as $wData) {
-            // Extract the data from the JSON payload
-            $widgetType = $wData['widgetType'] ?? null;
-            $widgetId = $wData['widgetId'] ?? null;
-            $col = $wData['x'] ?? null;
-            $row = $wData['y'] ?? null;
-            $sizeX = $wData['w'] ?? null;
-            $sizeY = $wData['h'] ?? null;
-            // Define the data to be updated or inserted
-            $data = [
-                'updated_at' => $updated,
-                'widget_type' => $widgetType,
-                'col' => $col,
-                'row' => $row,
-                'size_x' => $sizeX,
-                'size_y' => $sizeY
-            ];
+            $updated = date('Y-m-d H:i:s', time());
+            $existingWidgetIds = [];
+            foreach ($widgetDataList as $wData) {
+                // Extract the data from the JSON payload
+                $widgetType = $wData['widgetType'] ?? null;
+                $widgetId = $wData['widgetId'] ?? null;
+                $col = $wData['x'] ?? null;
+                $row = $wData['y'] ?? null;
+                $sizeX = $wData['w'] ?? null;
+                $sizeY = $wData['h'] ?? null;
+                // Define the data to be updated or inserted
+                $data = [
+                    'updated_at' => $updated,
+                    'widget_type' => $widgetType,
+                    'col' => $col,
+                    'row' => $row,
+                    'size_x' => $sizeX,
+                    'size_y' => $sizeY
+                ];
 
-            // Specify the conditions to search for existing records
-            $conditions = [
-                'widget_id' => $widgetId
-            ];
-            $existingWidgetIds[] = $widgetId;
+                // Specify the conditions to search for existing records
+                $conditions = [
+                    'widget_id' => $widgetId
+                ];
+                $existingWidgetIds[] = $widgetId;
 
-            // Check if the record already exists
-            if (!Layout::where($conditions)->exists()) {
-                // If the record doesn't exist, set the created_at timestamp
-                $data['created_at'] = $updated;
+                // Check if the record already exists
+                if (!Layout::where($conditions)->exists()) {
+                    // If the record doesn't exist, set the created_at timestamp
+                    $data['created_at'] = $updated;
+                }
+                Layout::updateOrInsert($conditions, $data);
             }
-            Layout::updateOrInsert($conditions, $data);
+            // Need to get widgets for staff that do not exist anymore and delete them...
+            if (sizeof($existingWidgetIds))
+                Layout::whereNotIn('widget_id', $existingWidgetIds)
+                    ->where('staff_id', $staffId)->delete();
+            else
+                Layout::where('staff_id', $staffId)->delete();
+        } catch (Exception $exception) {
+            return $exception->getMessage();
         }
-        // Need to get widgets for staff that do not exist anymore and delete them...
-        if (sizeof($existingWidgetIds))
-            Layout::whereNotIn('widget_id', $existingWidgetIds)
-                ->where('staff_id', $staffId)->delete();
-        else
-            Layout::where('staff_id', $staffId)->delete();
         return response()->json(['message' => 'Data saved successfully for staff id: ' . $staffId, 'updated_at' => $updated], 200);
     }
 }
--- resources/views/verified/dashboard.blade.php
+++ resources/views/verified/dashboard.blade.php
@@ -3,101 +3,107 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 @include('_partials._html_header')
 <body class="background-sizing gta-bg1">
-<div class="container-fluid master-contain">
-    <div class="row">
-        <div class="col col-auto px-0">
-            @include('_partials._sidebar')
-        </div>
-        <div class="col col-auto page d-flex">
-            <div class="row flex-grow-1">
-                <div class="col col-12">
-                    <div class="grid-stack">
-                        @foreach($data['widgetData'] as $widget)
-                            <div class="grid-stack-item" gs-y="{{$widget['row']}}" gs-x="{{$widget['col']}}"
-                                 gs-w="{{$widget['size_x']}}" gs-h="{{$widget['size_y']}}"
-                                 data-widgetType="{{$widget['widget_type']}}"
-                                 data-widgetId="{{$widget['widget_id']}}"
-                            >
-                                <div class="container-fluid h-100 px-0 grid-stack-item-content">
-                                    <div class="d-flex flex-column h-100">
-                                        <div class="row bg-zap mx-0 widget-header">
-                                            <div class="col-12 text-white">
-                                                {{ config('widget.WIDGET_NAMES_FROM_TYPE')[$widget['widget_type']] }}
+    <div class="container-fluid master-contain">
+        @include('_partials._toast')
+        <div class="row">
+            <div class="col col-auto px-0">
+                @include('_partials._sidebar')
+            </div>
+            <div class="col col-auto page d-flex">
+                <div class="row flex-grow-1">
+                    <div class="col col-12">
+                        <div class="grid-stack">
+                            @foreach($data['widgetData'] as $widget)
+                                <div class="grid-stack-item" gs-y="{{$widget['row']}}" gs-x="{{$widget['col']}}"
+                                     gs-w="{{$widget['size_x']}}" gs-h="{{$widget['size_y']}}"
+                                     data-widgetType="{{$widget['widget_type']}}"
+                                     data-widgetId="{{$widget['widget_id']}}"
+                                >
+                                    <div class="container-fluid h-100 px-0 grid-stack-item-content">
+                                        <div class="d-flex flex-column h-100">
+                                            <div class="row bg-zap mx-0 widget-header">
+                                                <div class="col-12 text-white">
+                                                    {{ config('widget.WIDGET_NAMES_FROM_TYPE')[$widget['widget_type']] }}
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div class="row mx-0 bg-white flex-grow-1">
-                                            <div class="col-12 text-dark justify-content-center align-middle d-flex align-items-center">
-                                                @include("_widgets." . $widget['widget_type'])
+                                            <div class="row mx-0 bg-white flex-grow-1">
+                                                <div class="col-12 text-dark justify-content-center align-middle d-flex align-items-center">
+                                                    @include("_widgets." . $widget['widget_type'])
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        @endforeach
+                            @endforeach
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+        @include('_partials._html_footer')
     </div>
-    @include('_partials._html_footer')
-</div>
-<script>
-    var csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-    $(function () { //DOM Ready
-        const grid = GridStack.init(
-            {
-                float: true,
-                alwaysShowResizeHandle: 'mobile',
-                cellHeight: 50,
-                disableDrag: true,
-                disableResize: true,
-                margin: 0,
-                removable: true,
-                removeTimeout: 3000
-            }
-        );
-        let customize = false;
-        $('#customize_val').on('change', () => {
-            grid.enableMove(!customize);
-            grid.enableResize(!customize);
-            customize = !customize;
-            if (!customize) {
-                const widgetDatas = [];
-                for (let gridItem of grid.getGridItems()) {
-                    const gridStackNode = gridItem?.gridstackNode;
-                    const gridEl = gridStackNode?.el;
-                    const widgetType = gridEl?.dataset?.widgettype;
-                    const widgetId = gridEl?.dataset?.widgetid;
-                    if (widgetType && gridStackNode) {
-                        // Valid widget type... We need to save
-                        const h = gridStackNode.h;
-                        const w = gridStackNode.w;
-                        const x = gridStackNode.x;
-                        const y = gridStackNode.y;
-                        const widgetData = {
-                            widgetType: widgetType,
-                            widgetId: widgetId,
-                            x: x,
-                            y: y,
-                            h: h,
-                            w: w
-                        };
-                        widgetDatas.push(widgetData);
-                    }
+    <script>
+        var csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+        $(function () { //DOM Ready
+            const grid = GridStack.init(
+                {
+                    float: true,
+                    alwaysShowResizeHandle: 'mobile',
+                    cellHeight: 50,
+                    disableDrag: true,
+                    disableResize: true,
+                    margin: 0,
+                    removable: true,
+                    removeTimeout: 3000
                 }
-                const opts = {
-                    method: 'PUT',
-                    headers: {
-                        'X-CSRF-TOKEN': csrfToken
-                    },
-                    body: JSON.stringify(widgetDatas)
-                };
-                fetch('dashboard/save', opts).then((resp) => {
-                    console.log("Response from saving =>", resp);
-                });
-            }
+            );
+            let customize = false;
+            $('#customize_val').on('change', () => {
+                grid.enableMove(!customize);
+                grid.enableResize(!customize);
+                customize = !customize;
+                if (!customize) {
+                    const widgetDatas = [];
+                    for (let gridItem of grid.getGridItems()) {
+                        const gridStackNode = gridItem?.gridstackNode;
+                        const gridEl = gridStackNode?.el;
+                        const widgetType = gridEl?.dataset?.widgettype;
+                        const widgetId = gridEl?.dataset?.widgetid;
+                        if (widgetType && gridStackNode) {
+                            // Valid widget type... We need to save
+                            const h = gridStackNode.h;
+                            const w = gridStackNode.w;
+                            const x = gridStackNode.x;
+                            const y = gridStackNode.y;
+                            const widgetData = {
+                                widgetType: widgetType,
+                                widgetId: widgetId,
+                                x: x,
+                                y: y,
+                                h: h,
+                                w: w
+                            };
+                            widgetDatas.push(widgetData);
+                        }
+                    }
+                    const opts = {
+                        method: 'PUT',
+                        headers: {
+                            'X-CSRF-TOKEN': csrfToken
+                        },
+                        body: JSON.stringify(widgetDatas)
+                    };
+                    fetch('dashboard/save', opts).then((resp) => {
+                        if (resp.status === 200) {
+                            // Successfully saved...
+                        } else {
+                            // Unsuccessful save, error encountered...
+                            showToast('error_toast', "Saving was unsuccessful...");
+                        }
+                    });
+                }
+            });
         });
-    });
-</script>
+    </script>
 </body>
 </html>
