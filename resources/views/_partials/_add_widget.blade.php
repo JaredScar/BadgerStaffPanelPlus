192bccfaa2dd6b0db75933fbd7b0ad09e1be5ea4
Jared <TheWolfBadger@gmail.com>
2024-03-15 22:50:01 -0400

    feat/dashboard-page-upgrades Adding add_widget partial blade file

---
+++ resources/views/_partials/_add_widget.blade.php
@@ -0,0 +1,36 @@
+<div class="container-fluid d-none" id="add_widget_screen">
+    <div class="row justify-content-center">
+        <div class="col-10">
+            <div class="row">
+                <!-- Bans -->
+                <div class="col-3">
+                    <i class="fa-solid fa-gavel"></i>
+                </div>
+                <!-- Commends -->
+                <div class="col-3">
+                    <i class="fa-solid fa-comment-dots"></i>
+                </div>
+                <!-- Kicks -->
+                <div class="col-3">
+                    <i class="fa-solid fa-shoe-prints"></i>
+                </div>
+                <!-- Warns -->
+                <div class="col-3">
+                    <i class="fa-solid fa-person-harassing"></i>
+                </div>
+                <!-- Notes -->
+                <div class="col-3">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                </div>
+                <!-- Records -->
+                <div class="col-3">
+                    <i class="fa-solid fa-clipboard"></i>
+                </div>
+                <!-- Trustscores -->
+                <div class="col-3">
+                    <i class="fa-solid fa-circle-check"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
