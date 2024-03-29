5da3305317a3a064846c97e6759dbf1ee04550dd
NarutoSage <51103195+NarutoSage@users.noreply.github.com>
2024-03-28 23:47:48 -0400

    Revised Database.blade.php (with install function)
    Need to complete "completed" function to complete this page.

--- resources/views/install/database.blade.php
+++ resources/views/install/database.blade.php
@@ -78,13 +78,13 @@
                 <div class="card">
                   <div class="card-header">Output Information</div>
                   <div class="row g-0">
-                    <div class="card-body">
-                      <table class="table table-hover">
+                    <div class="card-body overflow-auto d-flex" style="max-height: 150px; flex-direction: column-reverse;">
+                      <table class="table table-hover"> 
                         <thead>
                           <tr>
                             <th scope="col">Action #</th>
                             <th scope="col">SQL Function #</th>
-                            <th scope="col">Action</th>
+                            <th scope="col">Action/Result</th>
                           </tr>
                         </thead>
                         <tbody id="infolist" class="">
@@ -190,56 +190,161 @@
             startMigration.disabled = false;
 
             // function init database.
-            progressDescription.innerText = `Creating Database {{ $_ENV['DB_DATABASE']; }} `;
+            progressDescription.innerText = `Logging into {{ $_ENV['DB_DATABASE']; }} `;
             progressBar.style.width = '2%';
             progressTitle.innerText = progressBar.style.width;
             progressIcon.innerHTML = '<div class="spinner-border" role="status"><span class="visually-hidden">Loading...</span></div>'
-            progressTaskTitle.innerText = 'Creating Database';
-            progressTaskDescription.innerText = `We're creating your database/utilizing your database.`
+            progressTaskTitle.innerText = 'Logging In';
+            progressTaskDescription.innerText = `We're Logging in then creating/utilizing your database.`
             infolist.insertAdjacentHTML(`beforeEnd`, `<tr><th scope="row">1</th><td>Creating Database</td><td>Logging into Configured Database Server <code><div class="col d-flex flex-column mb-3"><div>Host: {{ $_ENV["DB_HOST"]; }}</div><div>Port: {{ $_ENV["DB_PORT"]; }}</div><div>Username: {{ $_ENV["DB_USERNAME"]; }}</div><div>Password: *******</div></div></code></td></tr>`)
-            attemptAndReact();
-            
-            function attemptAndReact() {
-              console.log('start');
-              var eventSource = new EventSource('/web/install/create-db');
-              console.log(eventSource);
-              eventSource.onmessage = function(event) {
-                console.log(event);
-                  var response = JSON.parse(event.data);
-                  response.forEach(function(status) {
-                    console.log(status);
-                    // report the changes in status live
-                    for (var res in status) {
-                      console.log(res, status[res]);
-                      if (res == 'status') {
-                        //report status bar color
-                        progressBar.classList.remove('bg-danger', 'bg-warning', 'bg-success'); 
-                        if (status[res] == 'success') { 
-                          progressBar.classList.add('bg-success');
-                          progressIcon.innerHTML = '<h1><i class="bi bi-check-circle"></i></h1>';
-                        }
-                        else if (status[res] == 'warning') { 
-                          progressBar.classList.add('bg-warning'); 
-                          progressIcon.innerHTML = '<h1><i class="bi bi-dash-square"></i></h1>';
-                        }
-                        else if (status[res] == 'error') { 
-                          progressBar.classList.add('bg-danger'); 
-                          progressIcon.innerHTML = '<h1><i class="bi bi-exclamation-square"></i></h1>';
-                        }
-                        
-                        // report percent change
-                        progressBar.style.width = `${status['percent']}%`;
-                        progressTitle.innerText = progressBar.style.width;
-                        
-                        //progressIcon.innerHTML = '<div class="spinner-border" role="status"><span class="visually-hidden">Loading...</span></div>';
-                      } else {
-                      }
-                    }
-                    })
-                }
-              }
+            attemptAndReact('login');
 
             });
+            async function attemptAndReact(executeThis) {
+              console.log('start');
+              try {
+                const response = await fetch('/web/install/create-db', { method: 'POST', headers: { 'Content-Type': 'application/json', 'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')}, body: JSON.stringify({ function_to_execute: executeThis})});
+                const data = await response;
+                const htmlData = await response.text();
+                for (const pair of data.headers.entries()) {
+                  console.log(pair[0] + ' ' + pair[1]);
+                }
+                if (typeof data === 'object') {
+                  // response is not json
+                  let currentIcon = document.getElementById('progress-icon');
+                  if (data['status'] === 500) {
+                    const errorMsg = await extractError(htmlData);
+                    console.log(errorMsg);
+                    if (errorMsg == "Unknown error occured") {
+                    console.log(response.headers.get('Content-Type'));
+                    console.log(htmlData);
+                    }
+                    if (errorMsg.includes('database exists')) {
+                      reportStats('success', 'Successfully Logged Into Database Server', '5', 'Successfully Logged Into Database Server', 'Success', 'Initiating Database', '<h1 class="text-success"><i class="bi bi-database-fill-check"></h1>', errorMsg);
+                      reportStats('warning', errorMsg, '6', 'table-staff', 'Warning', 'Unable to create database as it already exists, instead we will use the database if its empty.', '<h1 class="text-warning"><i class="bi bi-database-fill"></i></h1>', errorMsg);
+                      reportStats('info', 'Attempting to create Database Table "table-staff".', '7', 'Creating Table "staff"', 'Creating Staff', 'Creating "staff" Table within the database', currentIcon.innerHTML, 'Attempting to create Database Tables');
+                      setTimeout( function() {
+                        attemptAndReact('table-staff');
+                      }, 2000);
+                    } else {
+                      reportStats('error', errorMsg, 'x', `table-staff`, 'Error', 'Unable to complete Database Migration', '<h1 class="text-danger"><i class="bi bi-database-fill-x"></i></h1>', errorMsg);
+                    }
+                  }
+                  
+                  if(data['status'] === 200) {
+                    let newData = JSON.parse(htmlData);
+                    console.log(newData.percent);
+                    const spinnerDiv = '<div class="spinner-border" role="status"><span class="visually-hidden">Loading...</span></div>';
+                    reportStats('success', newData.taskdescription, newData.percent, newData.sqlfunction, newData.tasktitle, newData.description, newData.taskicon, newData.taskdescription)
+                    setTimeout( function() {
+                      if (newData.sqlfunction == "table-staff") {
+                        reportStats('info', 'Creating Database Table "users".', newData.percent, 'table-users', 'Creating users', 'Attempting to create the Database Table "users" within the database server.', spinnerDiv, 'Creating Database Table "users".');
+                      } else if (newData.sqlfunction == 'table-users') {
+                        reportStats('info', 'Altering Database Table "users".', newData.percent, 'table-users-two', 'Altering Users', 'Attempting to alter the Database Table "users" within the database server.', spinnerDiv, 'Altering Database Table "users".');
+                      } else if (newData.sqlfunction == 'table-users-two') {
+                        reportStats('info', 'Creating Database Table "warns".', newData.percent, 'table-warns', 'Creating Warns', 'Attempting to create the Database Table "warns" within the database server.', spinnerDiv, 'Creating Database Table "servers".');
+                      } else if (newData.sqlfunction == 'table-warns') {
+                        reportStats('info', 'Creating Database Table "bans".', newData.percent, 'table-bans', 'Creating Bans', 'Attempting to create the Database Table "bans" within the database server.', spinnerDiv, 'Creating Database Table "bans".');
+                      } else if (newData.sqlfunction == 'table-bans') {
+                        reportStats('info', 'Creating Database Table "commends".', newData.percent, 'table-commends', 'Creating Commends', 'Attempting to create the Database Table "commends" within the database server.', spinnerDiv, 'Creating Database Table "commends".');
+                      } else if (newData.sqlfunction == 'table-commends') {
+                        reportStats('info', 'Creating Database Table "failed_jobs".', newData.percent, 'table-failed-jobs', 'Creating Failed_jobs', 'Attempting to create the Database Table "failed_jobs" within the database server.', spinnerDiv, 'Creating Database Table "failed_jobs".');
+                      } else if (newData.sqlfunction == 'table-failed-jobs') {
+                        reportStats('info', 'Altering Database Table "failed_jobs".', newData.percent, 'table-failed-jobs-two', 'Altering Failed_jobs', 'Attempting to alter the Database Table "failed_jobs" within the database server.', spinnerDiv, 'Altering Database Table "failed_jobs".');
+                      } else if (newData.sqlfunction == 'table-failed-jobs-two') {
+                        reportStats('info', 'Creating Database Table "kicks".', newData.percent, 'table-kicks', 'Creating Kicks', 'Attempting to create the Database Table "kicks" within the database server.', spinnerDiv, 'Creating Database Table "kicks".');
+                      } else if (newData.sqlfunction == 'table-kicks') {
+                        reportStats('info', 'Creating Database Table "layouts".', newData.percent, 'table-layouts', 'Creating Layouts', 'Attempting to create the Database Table "layouts" within the database server.', spinnerDiv, 'Creating Database Table "layouts".');
+                      } else if (newData.sqlfunction == 'table-layouts') {
+                        reportStats('info', 'Creating Database Table "migrations".', newData.percent, 'table-migrations', 'Creating Migrations', 'Attempting to create the Database Table "migrations" within the database server.', spinnerDiv, 'Creating Database Table "migrations".');
+                      } else if (newData.sqlfunction == 'table-migrations') {
+                        reportStats('info', 'Creating Database Table "notes".', newData.percent, 'table-notes', 'Creating Notes', 'Attempting to create the Database Table "notes" within the database server.', spinnerDiv, 'Creating Database Table "notes".');
+                      } else if (newData.sqlfunction == 'table-notes') {
+                        reportStats('info', 'Creating Database Table "password_resets".', newData.percent, 'table-password-resets', 'Creating Password_resets', 'Attempting to create the Database Table "password_resets" within the database server.', spinnerDiv, 'Creating Database Table "password_resets".');
+                      } else if (newData.sqlfunction == 'table-password-resets') {
+                        reportStats('info', 'Creating Database Table "personal_access_tokens".', newData.percent, 'table-personal-access-tokens', 'Creating Personal_access_tokens', 'Attempting to create the Database Table "personal_access_tokens" within the database server.', spinnerDiv, 'Creating Database Table "personal_access_tokens".');
+                      } else if (newData.sqlfunction == 'table-personal-access-tokens') {
+                        reportStats('info', 'Altering Database Table "personal_access_tokens".', newData.percent, 'table-personal_access_tokens_two', 'Altering Personal_access_tokens', 'Attempting to alter the Database Table "personal_access_tokens" within the database server.', spinnerDiv, 'Altering Database Table "personal_access_tokens".');
+                      } else if (newData.sqlfunction == 'table-personal-access-tokens-two') {
+                        reportStats('info', 'Creating Database Table "players".', newData.percent, 'table-players', 'Creating Players', 'Attempting to create the Database Table "players" within the database server.', spinnerDiv, 'Creating Database Table "players".');
+                      } else if (newData.sqlfunction == 'table-players') {
+                        reportStats('info', 'Altering Database Table "players".', newData.percent, 'table-players-two', 'Altering Players', 'Attempting to alter the Database Table "players" within the database server.', spinnerDiv, 'Altering Database Table "players".');
+                      } else if (newData.sqlfunction == 'table-players-two') {
+                        reportStats('info', 'Creating Database Table "player_data".', newData.percent, 'table-player-data', 'Creating Player_data', 'Attempting to create the Database Table "player_data" within the database server.', spinnerDiv, 'Creating Database Table "player_data".');
+                      } else if (newData.sqlfunction == 'table-player-data') {
+                        reportStats('info', 'Altering Database Table "player_data".', newData.percent, 'table-player-data-two', 'Altering Player_data', 'Attempting to alter the Database Table "player_data" within the database server.', spinnerDiv, 'Creating Database Table "player_data".');
+                      } else if (newData.sqlfunction == 'table-player-data-two') {
+                        reportStats('info', 'Creating Database Table "servers".', newData.percent, 'table-servers', 'Creating servers', 'Attempting to create the Database Table "servers" within the database server.', spinnerDiv, 'Creating Database Table "servers".');
+                      } else if (newData.sqlfunction == 'table-servers') {
+                        reportStats('info', 'Verifying Database', newData.percent, 'completed', 'Verifying DB', 'Attempting to verify the database configuration (this may take a long time).', spinnerDiv, 'Verifying Database');
+                      } else if (newData.sqlfunction == 'completed') {
+                        reportStats('completed', 'Database Install Complete".', newData.percent, '', 'Database Install Complete', 'The Database Installation has been completed, you should now be able to continue to the next step.', spinnerDiv, 'Database Install Complete');
+                      }
+                      attemptAndReact(newData.newfunction);
+                    }, 2000);
+                  }
+                }
+              } catch (error) {
+                return console.log(error); 
+              }
+            }
+            function extractError(htmlString) {
+              console.log(htmlString);
+              const fetchDoc = new DOMParser().parseFromString(htmlString, 'text/html');
+              const errorElement = fetchDoc.querySelector('title');
+              return errorElement ? errorElement.textContent : 'Unknown error occured';
+            }
+            function reportStats(res, result, percentVal, sqlfunction, progressTaskTitleVal, progressDescriptionVal, progressIconVal, progressTaskDescriptionVal) {
+              let outputTable = document.getElementById('infolist');
+              let databaseProgressBar = document.getElementById('database-progress-bar');
+              let rowsNum = outputTable.rows.length - 1;
+              if (res == "error") {
+                outputTable.insertAdjacentHTML(`beforeEnd`, `<tr><th scope="row">${rowsNum} </th><td>${sqlfunction} \n <div class="my-2"><span class="bg-dark text-danger p-1 border border-danger"><b>Error</b></span></div></td><td>${result}</td></tr>`);
+
+                progressTaskTitle.innerText = progressTaskTitleVal;
+                progressDescription.innerText = progressDescriptionVal;
+                progressIcon.innerHTML = progressIconVal;
+                progressTaskDescription.innerText = progressTaskDescriptionVal;
+                databaseProgressBar.classList.remove('bg-success', 'bg-warning');
+                databaseProgressBar.classList.add('bg-danger');
+                progressBar.style.width = percentVal+'%';
+                progressTitle.innerText = progressBar.style.width;
+              } else if (res == "warning") {
+                outputTable.insertAdjacentHTML(`beforeEnd`, `<tr><th scope="row">${rowsNum} </th><td>${sqlfunction} \n <div class="my-2"><span class="bg-dark text-warning p-1 border border-warning"><b>Warning</b></span></div></td><td>${result}</td></tr>`)
+
+                progressTaskTitle.innerText = progressTaskTitleVal;
+                progressDescription.innerText = progressDescriptionVal;
+                progressIcon.innerHTML = progressIconVal;
+                progressTaskDescription.innerText = progressTaskDescriptionVal;
+                progressBar.style.width = percentVal+'%';
+                progressTitle.innerText = progressBar.style.width;
+                databaseProgressBar.classList.remove('bg-success', 'bg-danger');
+                databaseProgressBar.classList.add('bg-success');
+              } else if (res == "info") {
+                outputTable.insertAdjacentHTML(`beforeEnd`, `<tr><th scope="row">${rowsNum} </th><td>${sqlfunction} \n <div class="my-2"><span class="bg-dark text-info p-1 border border-info">Info</span></div></td><td>${result}</td></tr>`);
+
+                progressTaskTitle.innerText = progressTaskTitleVal;
+                progressDescription.innerText = progressDescriptionVal;
+                progressIcon.innerHTML = '<div class="spinner-border" role="status"><span class="visually-hidden">Loading...</span></div>';
+                progressTaskDescription.innerText = progressTaskDescriptionVal;
+                progressBar.style.width = percentVal+'%';
+                progressTitle.innerText = progressBar.style.width;
+
+              } else if (res == "success") {
+                outputTable.insertAdjacentHTML(`beforeEnd`, `<tr><th scope="row">${rowsNum}</th><td>${sqlfunction} \n <div class="my-2"><span class="bg-dark text-success p-1 border border-success">Success</span></div></td><td>${result}</td></tr>`);
+
+                progressTaskTitle.innerText = progressTaskTitleVal;
+                progressDescription.innerText = progressDescriptionVal;
+                progressIcon.innerHTML = `<h1 class="text-success"><i class="bi bi-${progressIconVal}"></i></h1>`;
+                progressTaskDescription.innerText = progressTaskDescriptionVal;
+                databaseProgressBar.classList.remove('bg-danger', 'bg-warning');
+                databaseProgressBar.classList.add('bg-success');
+                progressBar.style.width = percentVal+'%';
+                progressTitle.innerText = progressBar.style.width;
+              }
+              
+
+            }
         </script>
     </body>
 </html>
